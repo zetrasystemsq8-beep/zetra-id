9f4e22356b214c97b57dc55c4f9bd0917af2b97f
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-05 07:26:28 +0100

    Update auth_repository.dart

--- lib/features/authentication/domain/repositories/auth_repository.dart
+++ lib/features/authentication/domain/repositories/auth_repository.dart
@@ -1,6 +1,27 @@
-import '../entities/user.dart';
+import '../entities/user_entity.dart';
 
 abstract class AuthRepository {
-  Future<User?> signIn(String email, String password);
+  /// Creates a new Zetra account.
+  Future<UserEntity> createAccount(UserEntity user);
+
+  /// Signs in using Zetra ID or username.
+  Future<UserEntity?> signIn({
+    required String zetraIdOrUsername,
+    required String password,
+  });
+
+  /// Signs out the current user.
   Future<void> signOut();
+
+  /// Returns the currently signed-in user.
+  Future<UserEntity?> getCurrentUser();
+
+  /// Checks whether a username is already taken.
+  Future<bool> isUsernameAvailable(String username);
+
+  /// Checks whether a Zetra ID already exists.
+  Future<bool> isZetraIdAvailable(String zetraId);
+
+  /// Updates an existing user profile.
+  Future<UserEntity> updateProfile(UserEntity user);
 }
